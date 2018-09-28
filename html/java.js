f1e4a0d45826b7bb72d6f18321ec1ad8f8fabfaa
Colm Callan <colmcallan91@gmail.com>
2018-09-28 20:48:09 +0000

    first commit

---
+++ html/java.js
@@ -0,0 +1,47 @@
+
+
+// Read More Button for Osbourne
+
+$(document).ready(function() {
+  $("#Ozzy").on("hide.bs.collapse", function() {
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Read More');
+  });
+  $("#Ozzy").on("show.bs.collapse", function() {
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Read Less');
+  });
+});
+// Read More Button for Tony
+
+$(document).ready(function() {
+  $("#tony").on("hide.bs.collapse", function() {
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Read More');
+  });
+  $("#tony").on("show.bs.collapse", function() {
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Read Less');
+  });
+});
+
+// Read More Button for Geezer
+
+$(document).ready(function() {
+  $("#Geezer").on("hide.bs.collapse", function() {
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Read More');
+  });
+  $("#Geezer").on("show.bs.collapse", function() {
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Read Less');
+  });
+});
+
+
+// Read More Button for Ward
+$(document).ready(function() {
+  $("#Geezer").on("hide.bs.collapse", function() {
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Read More');
+  });
+  $("#Geezer").on("show.bs.collapse", function() {
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Read Less');
+  });
+});
+
+
+
